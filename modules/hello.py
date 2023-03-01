870700444ffc19709cb0cc96c70119ee163bc284
Erik Skoog <erik.skoog@medicalsolutions.com>
2023-03-01 13:50:54 -0600

    add index path

--- modules/hello.py
+++ modules/hello.py
@@ -2,6 +2,14 @@
 from app import app
 from flask import jsonify
 
+# Define route "/".
+
+
+@app.route('/')
+def index():
+    # return in JSON format. (For API)
+    return jsonify({"message": "Welcome to the Chatnurse Utils Server!"})
+
 # Define route "/hello".
 
 
